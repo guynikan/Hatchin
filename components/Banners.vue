e3ebfdd6f20207db27274acb7f72e36a9ac513d3
jr-lol <jr-lol@live.com>
2022-03-01 12:14:29 -0300

    criado componente de banners da home

--- components/Banners.vue
+++ components/Banners.vue
@@ -1,3 +1,46 @@
 <template>
-  <div>Banners</div>
+    <section class="container is-fullhd">
+        <b-carousel
+            :pause-info="pauseInfo"
+            :arrow="arrow"
+            :repeat="repeat"
+            :progress="progress"
+            :arrow-hover="arrowHover"
+            :icon-pack="iconPack"
+            :icon-prev="iconPrev"
+            :icon-next="iconNext"
+            :icon-size="iconSize">
+            <b-carousel-item v-for="(carousel, i) in carousels" :key="i">
+                <section :class="`hero is-medium is-${carousel.color}`">
+                    <div class="hero-body has-text-centered">
+                        <h1 class="title">{{carousel.title}}</h1>
+                    </div>
+                </section>
+            </b-carousel-item>
+        </b-carousel>
+    </section>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            pauseInfo: false,
+            arrow: false,
+            repeat: true,
+            arrowHover: false,
+            progress: true,
+            iconPack: 'mdi',
+            iconPrev: 'arrow-left',
+            iconNext: 'arrow-right',
+            iconSize: '',
+            carousels: [
+                { title: 'Slide 1', color: 'info' },
+                { title: 'Slide 2', color: 'success' },
+                { title: 'Slide 3', color: 'warning' },
+                { title: 'Slide 4', color: 'danger' }
+            ]
+        }
+    }
+}
+</script>
