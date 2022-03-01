0132e749b285ce9384698c91d8ae08bef0c2135c
jr-lol <jr-lol@live.com>
2022-03-01 12:14:34 -0300

    criado componente de brands

--- components/Brands.vue
+++ components/Brands.vue
@@ -1,3 +1,9 @@
 <template>
-  <div>Brands</div>
+  <section class="container is-fullhd">
+    <ul class="level py-6">
+      <li class="level-item has-text-centered">Sal e pimenta</li>
+      <li class="level-item has-text-centered">Palazzi</li>
+      <li class="level-item has-text-centered">Def</li>
+    </ul>
+  </section>
 </template>
