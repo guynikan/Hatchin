30d3e34d61f7e7c7b3b3be4497dc4b63cfb8188b
jr-lol <jr-lol@live.com>
2022-03-01 12:15:37 -0300

    criado componente de signup

--- pages/signup.vue
+++ pages/signup.vue
@@ -1,3 +1,30 @@
 <template>
-  <div>Signup</div>
+     <section class="container is-max-desktop">
+    <form class="box is-flex is-flex-direction-column">
+      <NuxtLink class="ml-auto mr-auto is-uppercase is-primary" to="/login">entrar</NuxtLink>
+      <hr>
+        <span class="ml-auto mr-auto">Ou</span>
+      <b-field label="E-mail">
+        <b-input
+          placeholder="joao@email.com">
+        </b-input>
+      </b-field>
+
+      <b-field label="Senha">
+        <b-input type="password"
+          placeholder="********"
+          password-reveal>
+        </b-input>
+        </b-field>
+
+        <b-button type="is-primary" native-type="submit" class="my-6 ml-auto mr-auto is-uppercase" outlined>cadastrar-se</b-button>
+        <NuxtLink class="ml-auto mr-auto" to="/password-recovery">Esqueci minha senha</NuxtLink>
+    </form>
+    </section>
 </template>
+<script>
+export default {
+  layout: "light"
+}
+</script>
+
