321d83fa4a97c524b9ccc1894d4002c7d76dcc7d
jr-lol <jr-lol@live.com>
2022-02-28 23:38:18 -0300

    compoente search do header

---
+++ components/Search.vue
@@ -0,0 +1,8 @@
+<template>
+  <b-field position="is-centered">
+    <b-input placeholder="Search..." type="search" icon-right="magnify">
+    </b-input>
+    <p>
+    </p>
+  </b-field>
+</template>
