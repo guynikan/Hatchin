95e32ab55c729e69e96af8ec1c08e646356649dc
jr-lol <jr-lol@live.com>
2022-03-01 12:15:32 -0300

    criado componente de password recovery

--- pages/password-recovery.vue
+++ pages/password-recovery.vue
@@ -1,3 +1,21 @@
 <template>
-  <div>Recuperar senha</div>
+     <section class="container is-max-desktop">
+    <form class="box is-flex is-flex-direction-column">
+      <NuxtLink class="ml-auto mr-auto is-uppercase is-primary" to="/login">Entrar</NuxtLink>
+      <hr>
+      <b-field label="E-mail">
+        <b-input
+          placeholder="joao@email.com">
+        </b-input>
+      </b-field>
+
+      <b-button type="is-primary" native-type="submit" class="my-6 ml-auto mr-auto is-uppercase" outlined>Solicitar recuperação</b-button>
+    </form>
+    </section>
 </template>
+<script>
+export default {
+  layout: "light"
+}
+</script>
+
