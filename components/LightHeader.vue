1ecbdb0b2ce5671389d0fc43c0e0f4380c3bf13b
jr-lol <jr-lol@live.com>
2022-03-01 12:14:57 -0300

    adicionada classe de fontsize ao title do header

--- components/LightHeader.vue
+++ components/LightHeader.vue
@@ -2,7 +2,9 @@
   <header class="mb-6">
     <div class="p-6 is-flex is-justify-content-center">
       <NuxtLink to="/">
-        EVABEL
+        <h1 class="is-size-1">
+          EVABEL
+        </h1>
       </NuxtLink>
     </div>
   </header>
