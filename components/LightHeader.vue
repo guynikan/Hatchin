11e8b4751367ee52e8bcba2f427a9c599f3073a3
jr-lol <jr-lol@live.com>
2022-02-28 23:38:13 -0300

    criados componentes da home

---
+++ components/LightHeader.vue
@@ -0,0 +1,9 @@
+<template>
+  <header class="mb-6">
+    <div class="p-6 is-flex is-justify-content-center">
+      <NuxtLink to="/">
+        EVABEL
+      </NuxtLink>
+    </div>
+  </header>
+</template>
---
+++ components/SectionCategory.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>SectionCategory</div>
+</template>
