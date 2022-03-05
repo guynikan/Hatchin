51acc2bb2339a14e203b7a7e0491fc57906ba6fb
jr-lol <jr-lol@live.com>
2022-03-05 11:32:16 -0300

    adicionada classe custom-input ao search

--- components/Search.vue
+++ components/Search.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="py-6">
     <b-field position="is-centered">
-      <b-input placeholder="Search..." type="search" icon-right="magnify">
+      <b-input custom-class="custom-input" placeholder="Search..." type="search" icon-right="magnify">
       </b-input>
       <p>
       </p>
