ead3bc8920285c261b39ef3ff94b1b9509bb2651
guynikan <jr-lol@live.com>
2022-04-24 13:32:21 -0300

    adjust sectioncategory to dealing with props

--- components/SectionCategory.vue
+++ components/SectionCategory.vue
@@ -2,25 +2,39 @@
   <section class="section my-6 container is-fullhd">
     <h1 class="is-size-2 mb-5">Jeans</h1>
 
-    <b-carousel-list v-model="test" class="is-shadowless" :data="items" :items-to-show="4">
+    <b-carousel-list
+      v-model="test"
+      class="is-shadowless"
+      :data="items"
+      :items-to-show="4"
+    >
       <template #item="list">
         <div class="card mx-2">
           <div class="card-image">
             <figure class="image is-3by5">
-              <a @click="info(list.index)"><img :src="list.image"></a>
+              <a :href="list.get_absolute_url"
+                ><img :src="mainImage(list.type.images)"
+              /></a>
             </figure>
-            <b-tag type="is-danger" rounded style="position: absolute; top: 0;"><b>50%</b></b-tag>
+            <b-tag type="is-danger" rounded style="position: absolute; top: 0"
+              ><b>50%</b></b-tag
+            >
           </div>
           <div class="card-content">
             <div class="content">
-              <p class="title is-6">{{ list.title }}</p>
+              <p class="title is-6">{{ list.name }}</p>
               <p class="subtitle is-7">@johnsmith</p>
-              <b-field grouped >
+              <b-field grouped>
                 <p v-if="list.rating" class="control">
-                  <b-rate :value="list.rating" show-score disabled/>
+                  <b-rate :value="list.rating" show-score disabled />
                 </p>
                 <p class="control" style="margin-left: auto">
-                  <b-button size="is-small" type="is-danger" icon-left="heart" outlined />
+                  <b-button
+                    size="is-small"
+                    type="is-danger"
+                    icon-left="heart"
+                    outlined
+                  />
                 </p>
               </b-field>
             </div>
@@ -32,57 +46,25 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
-    data() {
-        return {
-            test: 0,
-            items: [
-                {
-                    title: 'Slide 1',
-                    image: 'https://buefy.org/static/img/placeholder-1280x960.png',
-                    rating: 4.4
-                },
-                {
-                    title: 'Slide 2',
-                    image: 'https://buefy.org/static/img/placeholder-1280x960.png',
-                    rating: 3.5
-                },
-                {
-                    title: 'Slide 3',
-                    image: 'https://buefy.org/static/img/placeholder-1280x960.png',
-                    rating: 5
-                },
-                {
-                    title: 'Slide 4',
-                    image: 'https://buefy.org/static/img/placeholder-1280x960.png'
-                },
-                {
-                    title: 'Slide 5',
-                    image: 'https://buefy.org/static/img/placeholder-1280x960.png',
-                    rating: 5
-                },
-                {
-                    title: 'Slide 6',
-                    image: 'https://buefy.org/static/img/placeholder-1280x960.png',
-                    rating: 4
-                },
-                {
-                    title: 'Slide 7',
-                    image: 'https://buefy.org/static/img/placeholder-1280x960.png',
-                    rating: 2.7
-                },
-                {
-                    title: 'Slide 8',
-                    image: 'https://buefy.org/static/img/placeholder-1280x960.png',
-                    rating: 1.5
-                }
-            ]
-        }
+  props: {
+    items: {
+      type: Array,
+      default: () => [],
     },
-    methods: {
-        info(value) {
-            this.test = value
-        }
-    }
+  },
+
+  computed: {
+    ...mapGetters(['imageAbsoluteUrl']),
+  },
+  methods: {
+    mainImage(images) {
+      const imgDefault = images.find((img) => img.default).url
+
+      return this.imageAbsoluteUrl(imgDefault)
+    },
+  },
 }
 </script>
