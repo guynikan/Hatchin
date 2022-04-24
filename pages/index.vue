9644e7f2240d793ed53f90916d7688b31c4fae85
guynikan <jr-lol@live.com>
2022-04-24 13:31:32 -0300

    passing props to sectioncategory

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,42 @@
 <template>
   <div>
-    <Banners/>
-    <Brands/>
-    <LatestProducts/>
-    <SectionCategory/>
-    <SectionCategory/>
-    <Newsletter/>
+    <Banners />
+    <Brands />
+    <LatestProducts />
+    <SectionCategory :items="categoryOne" />
+    <SectionCategory :items="categoryTwo" />
+    <Newsletter />
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'HomePage',
-
   auth: false,
+
+  data() {
+    return {
+      categoryOne: [],
+      categoryTwo: [],
+    }
+  },
+
+  mounted() {
+    this.getProductsByCategory()
+  },
+
+  methods: {
+    getProductsByCategory() {
+      this.$axios.get(`/api/v1/product-category/1/`)
+      .then((products) => {
+          this.categoryOne = products.data
+        })
+
+      this.$axios.get(`/api/v1/product-category/2/`)
+      .then((products) => {
+          this.categoryTwo = products.data
+        })
+    },
+  },
 }
 </script>
