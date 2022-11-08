0adf6234905e0f0733cbd2b13df4efe473336d50
guynikan <jr-lol@live.com>
2022-11-08 15:55:37 -0300

    add fetch on latestProducts component

--- components/LatestProducts.vue
+++ components/LatestProducts.vue
@@ -2,34 +2,37 @@
   <section class="section my-6 container is-fullhd">
     <h1 class="is-size-2 mb-5">Os recém chegados</h1>
     <div class="tile is-ancestor">
-      <div class="tile is-vertical is-6">
+      <div v-if="$fetchState.pending">Loading...</div>
+      <div v-else class="tile is-vertical is-6">
         <div class="tile">
-          <div v-if="$fetchState.pending">Loading...</div>
-          <div
-            v-for="product in latestProducts"
-            v-else
-            :key="product.id"
-            class="tile is-parent is-vertical"
-          >
-            <NuxtLink :to="product.get_absolute_url">
-              <article class="tile is-child notification is-primary">
-                <p class="title">Vertical...</p>
-                <p class="subtitle">Top tile</p>
-              </article>
+          <div class="tile is-parent is-vertical">
+            <NuxtLink
+              v-for="product in firstHalf"
+              :key="product.id"
+              :to="product.get_absolute_url"
+              class="tile is-child notification is-primary"
+              :style="{
+                backgroundImage: `url(${getImage(product)})`,
+                backgroundPosition: '50% 50%',
+                backgroundSize: 'cover',
+              }"
+            >
             </NuxtLink>
           </div>
         </div>
       </div>
       <div class="tile is-parent">
-        <div v-if="$fetchState.pending">Loading...</div>
-        <article v-else class="tile is-child notification is-success">
-          {{ mainProduct }}
+        <article
+          class="tile is-child notification is-success"
+          :style="{
+            backgroundImage: `url(${imageAbsoluteUrl(mainProduct.image.url)})`,
+            height: '500px',
+            backgroundPosition: '50% 50%',
+            backgroundSize: 'cover',
+          }"
+        >
           <div class="content">
-            <p class="title">Tall tile</p>
-            <p class="subtitle">With even more content</p>
-            <div class="content">
-              <!-- Content -->
-            </div>
+            <div class="content"></div>
           </div>
         </article>
       </div>
@@ -37,22 +40,57 @@
   </section>
 </template>
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
-  name: 'Home',
+  name: 'LatestProducts',
   data() {
     return {
       latestProducts: [],
     }
   },
+
   async fetch() {
-    const response = await this.$axios.get('/api/v1/latest-products/')
-    this.latestProducts = await response.data
+    const products = await this.$axios.get('http://localhost:8000/api/v1/latest-products/')
+    this.latestProducts = products.data
   },
+
   computed: {
+    ...mapGetters(['imageAbsoluteUrl']),
+
+    firstHalf() {
+      const list = this.latestProducts
+      const middleIndex = Math.ceil(list.length / 2)
+
+      return list.splice(0, middleIndex)
+    },
+
+    secondHalf() {
+      const list = this.latestProducts
+      const middleIndex = Math.ceil(list.length / 2)
+
+      return list.splice(-middleIndex)
+    },
+
     mainProduct() {
       const products = this.latestProducts
-      console.log(`aaaaaaaaaaaaa`, products)
-      return products[0]
+      let img = { url: '/media/uploads/calca-alfaitaria.png' }
+
+      if (products[0] && products[0].type.images) {
+        img = products[0].type.images.find((img) => img.default)
+      }
+
+      return { name: '', image: img, ...products[0] }
+    },
+  },
+
+  methods: {
+    getImage(product) {
+      const {
+        type: { images },
+      } = product
+      const { url } = images.find((img) => img.default)
+      return this.imageAbsoluteUrl(url)
     },
   },
 }
