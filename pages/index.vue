f379e77d8afb9bb12cacc1ee38a61cc42507e043
guynikan <jr-lol@live.com>
2022-11-20 10:38:57 -0300

    fix axios call

--- pages/index.vue
+++ pages/index.vue
@@ -14,19 +14,18 @@ export default {
   name: 'HomePage',
   auth: false,
 
-  async asyncData({ $axios }) {
-    const latestProducts = await $axios.get(
-      'http://localhost:8000/api/v1/latest-products/'
-    )
+  asyncData({ $axios }) {
+    let latestProducts;
+    $axios.get(
+      '/api/v1/latest-products/'
+    ).then(({ data }) => { latestProducts = data})
 
-    const categoryOne = await $axios.get(`/api/v1/product-category/1/`)
+    // const categoryOne = await $axios.get(`/api/v1/product-category/1/`)
 
-    const categoryTwo = await $axios.get(`/api/v1/product-category/2/`)
+    // const categoryTwo = await $axios.get(`/api/v1/product-category/2/`)
 
     return {
       latestProducts,
-      categoryOne,
-      categoryTwo,
     }
   },
 }
